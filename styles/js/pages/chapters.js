712778991f3d14a1c58740811c2c9482739dd4b6
Juni <junikimdraws@gmail.com>
2026-03-31 01:39:56 -0500

    only do alla that if we profilin

--- styles/js/pages/chapters.js
+++ styles/js/pages/chapters.js
@@ -53,83 +53,82 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         console.log("DIALOGUE DATA:", chapterDialogue);
 
-      }
-  });
+        // -------------------------- //
+        // game initialization
+        // ---------------------------//
+        // select game objects
+        let $sprite = $('#sprite');;
+        let $background = $('#background');
+        let $speaker = $('#speaker');
+        let $speech = $('#speech');
 
-  // -------------------------- //
-  // game initialization
-  // ---------------------------//
-  // select game objects
-  let $sprite = $('#sprite');;
-  let $background = $('#background');
-  let $speaker = $('#speaker');
-  let $speech = $('#speech');
+        let scene = 0;
+        let order = 0;
 
-  let scene = 0;
-  let order = 0;
-
-  let char = 0; // number of characters visible
-  let charTimer;
+        let char = 0; // number of characters visible
+        let charTimer;
 
 
-  function updateView() {
-    const dialogue = chapterDialogue[scene][order];
-    $sprite.attr('src', characterSprites[dialogue.character][dialogue.emotion]);
-    $background.css('background-image',`url("${dialogue.background}")`);
-    $speaker.text(dialogue.character);
-    $speech.html(charadex.manageData.convertMarkdown(chapterDialogue[scene][order].text.slice(0, char)));
-    charTimer = setInterval(() => {
-      char += 2;
-      $speech.html(charadex.manageData.convertMarkdown(chapterDialogue[scene][order].text.slice(0, char)));
+        function updateView() {
+          const dialogue = chapterDialogue[scene][order];
+          $sprite.attr('src', characterSprites[dialogue.character][dialogue.emotion]);
+          $background.css('background-image',`url("${dialogue.background}")`);
+          $speaker.text(dialogue.character);
+          $speech.html(charadex.manageData.convertMarkdown(chapterDialogue[scene][order].text.slice(0, char)));
+          charTimer = setInterval(() => {
+            char += 2;
+            $speech.html(charadex.manageData.convertMarkdown(chapterDialogue[scene][order].text.slice(0, char)));
 
-      if (char >= chapterDialogue[scene][order].text.length) {
-        clearInterval(charTimer);
-      }
-    }, 50);
-  }
-
-  function sceneChange() {
-    // fade to black, change scene, then reveal again
-  }
-
-  $('#prev-button').on('click', function(e) {
-    e.preventDefault();
-    char = 0;
-    order -= 1;
-    if (order < 0) {
-      scene -= 1;
-      if (scene < 0) {
-        scene = 0;
-        order = 0;
-      } else {
-        order = chapterDialogue[scene].length - 1;
-      }
-    }
-    console.log("Prev clicked:", `${char}, ${order}, ${scene}`);
-    updateView();
-  });
-  $('#next-button').on('click', function(e) {
-    e.preventDefault();
-    if (char < chapterDialogue[scene][order].text.length) {
-      char = chapterDialogue[scene][order].text.length;
-    } else {
-      char = 0;
-      order += 1;
-      if (order >= chapterDialogue[scene].length) {
-        order = 0;
-        scene += 1;
-        if (scene >= Object.keys(chapterDialogue).length) {
-          scene = 0;
+            if (char >= chapterDialogue[scene][order].text.length) {
+              clearInterval(charTimer);
+            }
+          }, 50);
         }
+
+        function sceneChange() {
+          // fade to black, change scene, then reveal again
+        }
+
+        $('#prev-button').on('click', function(e) {
+          e.preventDefault();
+          char = 0;
+          order -= 1;
+          if (order < 0) {
+            scene -= 1;
+            if (scene < 0) {
+              scene = 0;
+              order = 0;
+            } else {
+              order = chapterDialogue[scene].length - 1;
+            }
+          }
+          console.log("Prev clicked:", `${char}, ${order}, ${scene}`);
+          updateView();
+        });
+        $('#next-button').on('click', function(e) {
+          e.preventDefault();
+          if (char < chapterDialogue[scene][order].text.length) {
+            char = chapterDialogue[scene][order].text.length;
+          } else {
+            char = 0;
+            order += 1;
+            if (order >= chapterDialogue[scene].length) {
+              order = 0;
+              scene += 1;
+              if (scene >= Object.keys(chapterDialogue).length) {
+                scene = 0;
+              }
+            }
+          }
+          console.log("Next clicked:", `${char}, ${order}, ${scene}`);
+          updateView();
+        });
+
+        // show page
+        updateView(chapterDialogue[scene][order]);
+
       }
-    }
-    console.log("Next clicked:", `${char}, ${order}, ${scene}`);
-    updateView();
   });
-
-  // show page
-
-  updateView(chapterDialogue[scene][order]);
 
   charadex.tools.loadPage('.softload', 100);
 });
