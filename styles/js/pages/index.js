8f6f5e33495ac6167bf5a7a0e583db997cd4300b
Juni <junikimdraws@gmail.com>
2026-04-01 11:57:13 -0500

    remove changes

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -13,17 +13,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   ===================================================================== */
   let prompts = await charadex.initialize.page(null, charadex.page.index.prompts, (arr) => {
 
-    // Sort the array in configured order
-    if (charadex.page.index.prompts.sort?.toggle ?? false) {
-      arr = charadex.manageData.sortArray(
-        arr, 
-        charadex.page.index.prompts.sort.sortProperty, 
-        charadex.page.index.prompts.sort.order,
-        charadex.page.index.prompts.sort.parametersKey,
-        charadex.page.index.prompts.sort.parameters,
-      );
-    }
-
       // Splice the silly little array
       let sliceAmount = charadex.page.index.prompts.amount || 4;
       arr.splice(sliceAmount, arr.length);
@@ -46,16 +35,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   ===================================================================== */
   let designs = await charadex.initialize.page(null, charadex.page.index.designs, (arr) => {
     
-    // Sort the array in configured order
-    if (charadex.page.index.designs.sort?.toggle ?? false) {
-      arr = charadex.manageData.sortArray(
-        arr, 
-        charadex.page.index.designs.sort.sortProperty, 
-        charadex.page.index.designs.sort.order,
-        charadex.page.index.designs.sort.parametersKey,
-        charadex.page.index.designs.sort.parameters,
-      );
-    }
+    // Sort the array in desc order
+    arr.sort((a,b) => b.dateadded - a.dateadded);
 
     // Splice the silly little array
     let sliceAmount = charadex.page.index.designs.amount || 6;
