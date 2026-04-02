462015dd16a0c5adc5736adfb90c73d5478acde9
Juni <junikimdraws@gmail.com>
2026-04-02 12:27:19 -0500

    add json handling for rels

--- styles/js/pages/characters.js
+++ styles/js/pages/characters.js
@@ -49,58 +49,111 @@ document.addEventListener("DOMContentLoaded", async () => {
         // Oh lordt, it's rels time =========================================
         if (profile.relationships && typeof profile.relationships === 'string') {
 
-          // our rels column has a textjoin of all relationships
-          // we need to put it back into array form
-          let relSplit = profile.relationships.split(';;;');
-          
-          const numCols = 8; // Name, Hide,	Updated,	Network Lvl,	Relationship,	Bonus,	Bonus Description,	Description
-          let relElement = '';
+          // old way of doing this
+          if (profile.relationships.includes(';;;')) {
+            // our rels column has a textjoin of all relationships
+            // we need to put it back into array form
+            let relSplit = profile.relationships.split(';;;');
+            
+            const numCols = 8; // Name, Hide,	Updated,	Network Lvl,	Relationship,	Bonus,	Bonus Description,	Description
+            let relElement = '';
 
-          for (let i = 0; i < relSplit.length; i += numCols) {
-            let rel = relSplit.slice(i, i + numCols);
+            for (let i = 0; i < relSplit.length; i += numCols) {
+              let rel = relSplit.slice(i, i + numCols);
 
-            if (rel[1] === 'FALSE') { // hiding = FALSE
-              // Set the character link
-              const charLink = charadex.url.addUrlParameters(
-                charadex.url.getPageUrl(charadex.page.masterlist.sitePage),
-                { profile: charadex.tools.scrub(rel[0]) });
-              const relTitle = rel[4] ? rel[4] : '--';
-              const relText = rel[7] ? charadex.manageData.convertMarkdown(rel[7]) : `<span class="text-muted">--</span>`;
-              const bonusTitle = rel[5] ? rel[5] : '--';
-              const bonusText = rel[6] ? charadex.manageData.convertMarkdown(rel[6]) : `<span class="text-muted">--</span>`;
-              let networked = '';
-              if (!isNaN(rel[3])) {
-                const pipfilled = `<i class="fa-solid fa-circle fa-xs"></i>`
-                const pipempty = `<i class="fa-regular fa-circle fa-xs"></i>`
-                networked = pipfilled.repeat(Number(rel[3])) + pipempty.repeat(9 - Number(rel[3]));
+              if (rel[1] === 'FALSE') { // hiding = FALSE
+                // Set the character link
+                const charLink = charadex.url.addUrlParameters(
+                  charadex.url.getPageUrl(charadex.page.masterlist.sitePage),
+                  { profile: charadex.tools.scrub(rel[0]) });
+                const relTitle = rel[4] ? rel[4] : '--';
+                const relText = rel[7] ? charadex.manageData.convertMarkdown(rel[7]) : `<span class="text-muted">--</span>`;
+                const bonusTitle = rel[5] ? rel[5] : '--';
+                const bonusText = rel[6] ? charadex.manageData.convertMarkdown(rel[6]) : `<span class="text-muted">--</span>`;
+                let networked = '';
+                if (!isNaN(rel[3])) {
+                  const pipfilled = `<i class="fa-solid fa-circle fa-xs"></i>`
+                  const pipempty = `<i class="fa-regular fa-circle fa-xs"></i>`
+                  networked = pipfilled.repeat(Number(rel[3])) + pipempty.repeat(9 - Number(rel[3]));
+                }
+                // Create the DOM elements
+                relElement += `<div class="col-md-4 col-12 p-2">
+                                <div class="card bg-body-tertiary h-100">
+                                  <div class="card-header text-center d-flex">
+                                    <div class="m-auto z-1">
+                                      <a href="${charLink}"><h3 class="card-title mb-0">${rel[0]} (${relTitle})</h3></a>
+                                    </div>
+                                  </div>
+                                  <div class="card-body d-flex flex-column flex-fill">
+                                    <div>${relText}</div>
+                                    <div class="alert alert-warning mt-2 mb-0 px-3 py-2">
+                                      <h3 class="span-header text-center text-warning mb-0">${bonusTitle}</h3>
+                                      <div class="text-center text-warning">${networked}</div>
+                                      <div>${bonusText}</div>
+                                    </div>
+                                  </div>
+                                  <div class="card-footer text-muted small">
+                                    <div class="row">
+                                      <div class="col">Last Updated:</div>
+                                      <div class="col-auto">${rel[2]}</div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>`
               }
-              // Create the DOM elements
-              relElement += `<div class="col-md-4 col-12 p-2">
-                              <div class="card bg-body-tertiary h-100">
-                                <div class="card-header text-center d-flex">
-                                  <div class="m-auto z-1">
-                                    <a href="${charLink}"><h3 class="card-title mb-0">${rel[0]} (${relTitle})</h3></a>
+            }
+            $('#rel-container').html(relElement);
+
+          } else {
+            let relJSON = JSON.parse(profile.relationships);
+            let relElement = '';
+            // Name, Hide,	Updated,	Network Lvl,	Relationship,	Bonus,	Bonus Description,	Description
+            for (let ix in relJSON['Name']) {
+              if (relJSON['Name'][ix] != '' && relJSON['Hide'][ix] === 'FALSE') {
+                // Set the character link
+                const charLink = charadex.url.addUrlParameters(
+                  charadex.url.getPageUrl(charadex.page.masterlist.sitePage),
+                  { profile: charadex.tools.scrub(relJSON['Name'][ix]) });
+                const relTitle = relJSON['Relationship'][ix] ? relJSON['Relationship'][ix] : '--';
+                const relText = relJSON['Description'][ix] ? charadex.manageData.convertMarkdown(relJSON['Description'][ix]) : `<span class="text-muted">--</span>`;
+                const bonusTitle = relJSON['Bonus'][ix] ? relJSON['Bonus'][ix] : '--';
+                // const bonusText = rel[6] ? charadex.manageData.convertMarkdown(rel[6]) : `<span class="text-muted">--</span>`;
+                let networked = '';
+                if (!isNaN(relJSON['Network Lvl'][ix])) {
+                  const pipfilled = `<i class="fa-solid fa-circle fa-xs"></i>`
+                  const pipempty = `<i class="fa-regular fa-circle fa-xs"></i>`
+                  networked = pipfilled.repeat(Number(relJSON['Network Lvl'][ix])) + pipempty.repeat(9 - Number(relJSON['Network Lvl'][ix]));
+                }
+                // Create the DOM elements
+                relElement += `<div class="col-md-4 col-12 p-2">
+                                <div class="card bg-body-tertiary h-100">
+                                  <div class="card-header text-center d-flex">
+                                    <div class="m-auto z-1">
+                                      <a href="${charLink}"><h3 class="card-title mb-0">${relJSON['Name'][ix]} (${relTitle})</h3></a>
+                                    </div>
+                                  </div>
+                                  <div class="card-body d-flex flex-column flex-fill">
+                                    <div>${relText}</div>
+                                    <div class="alert alert-warning mt-2 mb-0 px-3 py-2">
+                                      <h3 class="span-header text-center text-warning mb-0">${bonusTitle}</h3>
+                                      <div class="text-center text-warning">${networked}</div>
+                                      <div>${bonusText}</div>
+                                    </div>
+                                  </div>
+                                  <div class="card-footer text-muted small">
+                                    <div class="row">
+                                      <div class="col">Last Updated:</div>
+                                      <div class="col-auto">${relJSON['Updated'][ix]}</div>
+                                    </div>
                                   </div>
                                 </div>
-                                <div class="card-body d-flex flex-column flex-fill">
-                                  <div>${relText}</div>
-                                  <div class="alert alert-warning mt-2 mb-0 px-3 py-2">
-                                    <h3 class="span-header text-center text-warning mb-0">${bonusTitle}</h3>
-                                    <div class="text-center text-warning">${networked}</div>
-                                    <div>${bonusText}</div>
-                                  </div>
-                                </div>
-                                <div class="card-footer text-muted small">
-                                  <div class="row">
-                                    <div class="col">Last Updated:</div>
-                                    <div class="col-auto">${rel[2]}</div>
-                                  </div>
-                                </div>
-                              </div>
-                             </div>`
+                              </div>`
+              }
+            }
+            $('#rel-container').html(relElement);
+              
             }
           }
-          $('#rel-container').html(relElement);
         }
       }
   });
